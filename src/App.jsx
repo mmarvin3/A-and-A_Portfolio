96c5767feb2c5df836c609d62c6cf905b0e05695
mmarvin3 <mmerida336@gmail.com>
2023-09-18 18:56:45 -0400

    Initial set up of page components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <About/>
+      <Projects/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
