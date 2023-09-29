343b418eabacab25eb3007917b8c84deaf02ebeb
mmarvin3 <mmerida336@gmail.com>
2023-09-29 00:00:38 -0400

    Initial page components built and set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <About/>
-      <Projects/>
-      <Contact/>
+      <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/projects' element={<Projects />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
